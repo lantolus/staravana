5d03c55790fdb032d213cb10a64fedbb27264a6a
lantolus <antollukas6@gmail.com>
2025-06-18 19:17:10 +0200

    responsivity added

---
+++ js/hamburger.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) 
+      $("#drop").addClass("active");
+    else if (scroll < 80) 
+      $("#drop").removeClass("active");
+  });
+  
+  $("#drop, nav ~ ul li").click(function() {
+    $("nav, #drop span").toggleClass("open");
+    $("body").toggleClass("hidden");
+    // $(window).scrollTop(0); //scroll on top after click on menu
+  });
